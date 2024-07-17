b67f6f7b711a7ce5b7beb19ff6975165a7663237
backdevjung <jcy0011@yahoo.com>
2024-07-18 02:52:34 +0900

    Initial commit

---
+++ First_GUI.py
@@ -0,0 +1,20 @@
+
+
+
+
+
+#======================================
+# imports
+#======================================
+import tkinter as tk
+
+# Create instance
+win = tk.Tk()
+
+# Add a title
+win.title("Python GUI")
+
+#======================================
+# Start GUI
+#======================================
+win.mainloop()
