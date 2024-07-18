9728dab0994505d4d75dd513b9554348d30b525f
backdevjung <jcy0011@yahoo.com>
2024-07-19 01:53:32 +0900

    Add a label
    Grid layout manager

--- GUI_add_label.py
+++ GUI_add_label.py
@@ -7,6 +7,7 @@
 # imports
 #======================================
 import tkinter as tk
+from tkinter import ttk
 
 # Create instance
 win = tk.Tk()
@@ -14,8 +15,11 @@ win = tk.Tk()
 # Add a title
 win.title("Python GUI")
 
+# Adding a Label
+ttk.Label(win, text="A Label").grid(column=0, row=0)
+
 # Disable resizing the GUI by passing in False/False (width/height)
-win.resizable(False, False)
+# win.resizable(False, False)
 
 #======================================
 # Start GUI
